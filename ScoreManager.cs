42b61fa709528b86bc8a5067f3cda31acc071ef8
Kirill.Melnikov <dezxplay@gmail.com>
2025-05-06 23:32:06 +0300

    score manager added

---
+++ ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class ScoreManager
+    {
+        public int Score { get; private set; }
+
+        public void AddPoints(int amount)
+        {
+            Score += amount;
+        }
+
+        public void SaveScore(string playerName)
+        {
+            string line = $"{playerName};{Score};{DateTime.Now}";
+            File.AppendAllText("results.txt", line + Environment.NewLine);
+        }
+
+        public static string[] GetTopResults(int count)
+        {
+            if (!File.Exists("results.txt"))
+                return new string[0];
+
+            var lines = File.ReadAllLines("results.txt");
+            Array.Sort(lines, (a, b) =>
+                int.Parse(b.Split(';')[1]).CompareTo(int.Parse(a.Split(';')[1]))); // по убыванию очков
+            return lines.Length > count ? lines[..count] : lines;
+        }
+
+        public void Reset()
+        {
+            Score = 0;
+        }
+    }
+}
